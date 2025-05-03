11eca3b3ef459ccc077bf88b8a7739040496cc28
Aayush Arora <aayusharoralinkedin@gmail.com>
2025-05-03 12:03:25 +0530

    Create Room

--- Bouncy Motors/Assets/Game Assets/Scripts/PhotonNetworkManager.cs
+++ Bouncy Motors/Assets/Game Assets/Scripts/PhotonNetworkManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Photon.Pun;
-using System.Collections;
+using Photon.Realtime;
+
 public class PhotonNetworkManager : MonoBehaviourPunCallbacks
 {
     public static PhotonNetworkManager Instance;
@@ -21,13 +22,40 @@ public class PhotonNetworkManager : MonoBehaviourPunCallbacks
         PhotonNetwork.NickName = playerName;
     }
 
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("Lobby joined!");
+    }
+
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         Debug.Log("Photon is connected!");
         GameManager.instance.OpenEMenu(MenuName.MainMenu);
+        PhotonNetwork.SendRate = 30;
+        PhotonNetwork.SerializationRate = 60;
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.JoinLobby();
     }
 
+    public void CreateRoom(string roomName, RoomOptions roomOptions)
+    {
+        PhotonNetwork.CreateRoom(roomName, roomOptions);
+        GameManager.instance.OpenEMenu(MenuName.MainMenu);
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("Room created!");
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Room joined!");
+    }
 }
--- Bouncy Motors/Assets/Game Assets/Scripts/UIManager.cs
+++ Bouncy Motors/Assets/Game Assets/Scripts/UIManager.cs
@@ -1,11 +1,26 @@
+using Photon.Realtime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Nickname Lobby")]
+
     public TMP_InputField playerNickname;
     public Button playBtn;
+
+    [Header("Main Lobby")]
+
+    public Button roomCreateBtn;
+    public Button findRoomBtn;
+    public Button quitBtn;
+
+    [Header("CreateRoom Lobby")]
+
+    public TMP_InputField roomName;
+    public Slider playersSlider;
+    public Button createRoomBtn;
 
     private void Awake()
     {
@@ -23,10 +38,39 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         playBtn.onClick.AddListener(OnPlayButtonPressed);
+        roomCreateBtn.onClick.AddListener(OnCreateRoomButtonPressed);
+        findRoomBtn.onClick.AddListener(OnFindRoomButtonPressed);
+        quitBtn.onClick.AddListener(OnQuitRoomPressed);
+        createRoomBtn.onClick.AddListener(OnRoomCreatedButtonPressed);
     }
 
     private void OnPlayButtonPressed()
     {
         PhotonNetworkManager.Instance.ConnectedToServer(playerNickname.text);
+    }
+
+    private void OnCreateRoomButtonPressed()
+    {
+        GameManager.instance.OpenEMenu(MenuName.CreateRoom);
+    }
+
+    private void OnFindRoomButtonPressed()
+    {
+        GameManager.instance.OpenEMenu(MenuName.FindRoom);
+    }
+
+    private void OnRoomCreatedButtonPressed()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = (int)playersSlider.value;
+        roomOptions.EmptyRoomTtl = 30000;
+        GameManager.instance.OpenEMenu(MenuName.WaitingList);
+    }
+
+    private void OnQuitRoomPressed()
+    {
+        Application.Quit();
     }
 }
